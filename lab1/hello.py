2c47caa1256c5667a41f144c516325ba53056781
Samuel Masih <sidhusamuel567@gmail.com>
2026-07-31 08:47:09 +0530

    updated

--- lab1/hello.py
+++ lab1/hello.py
@@ -9,3 +9,23 @@ print(name+verb+relation)
 age=20
 end="years old"
 print(name,age,end)
+
+print(3*"Samuel")
+for i in range(3):
+    print("Samuel")
+
+#assignment 2.1
+def add(a,b,c):
+    return a+b+c
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+result=add(a,b,c)
+print("addition:", result)
+
+for i in range(10):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
