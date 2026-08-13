5131d31acefe5a36325b5ee6d4b57eaf07819bd3
Samuel Masih <sidhusamuel567@gmail.com>
2026-08-13 20:51:19 +0530

    assignment 1-6

--- lab1/hello.py
+++ lab1/hello.py
@@ -24,8 +24,21 @@ c=int(input("enter third number"))
 result=add(a,b,c)
 print("addition:", result)
 
+print("printing 10 numbers")
 for i in range(10):
     print(i)
 
+print("printing 1-10 with by 2 gaps")
 for i in range(1,10,2):
     print(i)
+
+print("while Loop")
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+for i in range(1,11):
+    print("5 *",i,"=",5*i)
+
+print(*range(1,10))
